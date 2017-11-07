5d8f6582b56c3295242d27c8253a05d50154ecf8
yairdaon <yair.daon@gmail.com>
2017-11-06 16:45:36 -0800

    changed model.r so that now it runs with all combinations and library choices and saves the weighted predictions so I can mess with it without having to re-run all that shit

--- model/model.r
+++ model/model.r
@@ -12,15 +12,15 @@ source( "../helpers/helper.r" )
 ## filename = paste0("http://science.sciencemag.org/highwire/filestream/683325/",
 ##                   "field_highwire_adjunct_files/1/aag0863_SupportingFile_Suppl._Excel_seq1_v2.xlsx")
 
-predict <- function(raw_df,
-                    variables = names(raw_df), 
-                    E = 3, ## Embedding dimension of the system.
-                    n_lags = 3, ## 0, -1,..., -(n_lags-1)
-                    n_samp = 5, ## Number of random libraries, should be in the hundreds
-                    lib_range = c(501:2001),  ## Library set.
-                    pred = c(2501,3000), ## Prediciton set.
-                    lib_sizes = (1:3)*10     ## Library changes in size and is also random
-                    )
+save_weighted_predictions <- function(raw_df,
+                                      variables = names(raw_df), 
+                                      E = 3, ## Embedding dimension of the system.
+                                      n_lags = 3, ## 0, -1,..., -(n_lags-1)
+                                      n_samp = 5, ## Number of random libraries, should be in the hundreds
+                                      lib_range = c(501:2001),  ## Library set.
+                                      pred = c(2501,3000), ## Prediciton set.
+                                      lib_sizes = (1:3)*10     ## Library changes in size and is also random
+                                      )
 {   
     ## Immediately rescale the data frame and keep track of the
     ## normalizing factors.
@@ -31,38 +31,29 @@ predict <- function(raw_df,
     ## number of variables in the dynamical system
     n_vars <- ncol(df)
 
-    ## Preallocate 
-    results <- data.frame(lib_sizes = lib_sizes)
-    results$avg <- numeric(nrow(results))
-    results$top <- numeric(nrow(results))
-    results$med <- numeric(nrow(results))
-    results$bot <- numeric(nrow(results))
-
     ## Create lags for every variable and make y_p1
     df <- lag_every_variable(df, n_lags)
-    
 
     ## Create the list of combinations
     combinations <- make_combinations(n_vars, n_lags, E)
-    n_comb <- ncol(combinations)
+    n_comb       <- ncol(combinations)
     
     ## Preallocate memory for predictions and uncertainties
     pred_size   <- pred[2] - pred[1] + 1
     pred_table  <- matrix( NA,  nrow = n_comb, ncol = pred_size )
     var_table   <- matrix( Inf, nrow = n_comb, ncol = pred_size )
-    rhos        <- rep   ( NA,         n_samp                   )
-    prediction  <- numeric(                           pred_size )
+    predictions <- matrix( NA,  nrow = n_samp, ncol = pred_size )
 
     ## Do the analysis for every variable. 
     for( curr_var in variables )
     {
-        for (lib_ind in 1:nrow(results) )
+        for (lib_size in lib_sizes)
         {
             ## For every random library starting point
             for( smp in 1:n_samp )
             {
                 ## Choose random lib
-                lib <- random_lib(lib_range, lib_sizes[lib_ind])
+                lib <- random_lib(lib_range, lib_size)
                 
                 for( comb in 1:n_comb )
                 {
@@ -74,68 +65,45 @@ predict <- function(raw_df,
                     ## Variables used for prediction are chosen
                     ## according to the current combinations. Since
                     ## first cols are x_p1, y_p1, z_p1, we skip them
-                    ## and add n_vars.
-                    cols <- combinations[, comb] + n_vars
-                    target_column <- paste0( curr_var, "_p1" )
-
+                    ## and add n_var.
                     output <- block_lnlp(df,
                                          lib = lib,   ## lib is chosen randomly above
                                          pred = pred, ## pred is ALWAYS the same
                                          method = "simplex",
                                          tp = 0, ## Lags built into df
-                                         columns = cols, 
-                                         target_column = target_column,
+                                         columns = combinations[, comb] + n_vars, 
+                                         target_column = paste0( curr_var, "_p1" ),
                                          first_column_time = FALSE,
                                          short_output = FALSE,
                                          stats_only = FALSE )
 
                     ## time <- output[[1]]$model_output$time
-                    pr   <- output[[1]]$model_output$pred[pred[1]:pred[2]]
-                    vars <- output[[1]]$model_output$pred_var[pred[1]:pred[2]]
+                    ## pr   <- output[[1]]$model_output$pred[pred[1]:pred[2]]
+                    ## vars <- output[[1]]$model_output$pred_var[pred[1]:pred[2]]
 
                     ## rho <- output[[1]]$stats$rho
                     ## calc_rho <- cor(pr, df[time,var_ind], use = "complete.obs" ) 
-                    ## Not sure why these are not exactly equal...
+                    ## Not sure why these are equal only up to 1e-15 error...
                     ## if(  abs(calc_rho-rho) > 1e-14 )
                     ##     stop( calc_rho - rho )
 
-                    ## Save ALL data.
-                    pred_table[comb, ] <- pr 
-                    var_table [comb, ] <- vars
+                    ## These tables hold data for the current (random) library
+                    pred_table[comb, ] <- output[[1]]$model_output$pred    [pred[1]:pred[2]] 
+                    var_table [comb, ] <- output[[1]]$model_output$pred_var[pred[1]:pred[2]]
                 }
 
-
-                mu  <- mus$curr_var
-                sig <- sigs$curr_var
-                
-                prediction <- weighted_prediction(var_table, pred_table)
-                truth <- df[ pred[1]:pred[2], curr_var ]
-
-                ## We don't really need to shift and rescale variables
-                ## back, since the correlation is invariant to shifts
-                ## and scaling (when operations performed on both
-                ## variables).
-                prediction <- descale(prediction, mu, sig)
-                truth      <- descale(truth, mu, sig)
-
-                ## Store the correlation coefficient
-                rhos[smp]  <- cor(prediction, truth, use = "complete.obs" )
-
+                ## Weighted predictions for the current sampled library 
+                predictions[smp, ] <- weighted_prediction(var_table, pred_table)
+                ## prediction <- descale(prediction, mus$curr_var, sigs$curr_var)
             }
 
-            quarts <- quantile(rhos, probs = c(0.25,0.5,0.75))
-
-            results$avg[lib_ind] <- mean(rhos)
-            results$bot[lib_ind] <- quarts[1]
-            results$med[lib_ind] <- quarts[2]
-            results$top[lib_ind] <- quarts[3]
+            write.table(predictions,
+                        file = paste0("data/all_predictions_", curr_var, "_lib_size_", lib_size, ".csv"),
+                        quote = FALSE,
+                        na = "NA",
+                        row.names = FALSE,
+                        col.names = FALSE)
         }
-
-        write.csv(results,
-                  file = paste0("data/rhos_", curr_var, ".csv"),
-                  quote = FALSE,
-                  na = "NA",
-                  row.names = TRUE)        
     }
 }
 
@@ -145,18 +113,14 @@ df <- read.csv("originals/data.csv",
                      header = TRUE,
                      sep = "," )
 
-variables <- c( "x" )
+variables <- c( "x", "y", "z" )
 
-predict(df,
-        variables = variables,
-        E = 3, ## Embedding dimension of the system.
-        n_lags = 3, ## 0, -1,..., -(n_lags-1)
-        n_samp = 150, ## Number of random libraries, should be in the hundreds
-        lib = c(501,2001),  ## Library set.
-        pred = c(2501,3000), ## Prediciton set.
-        lib_sizes = (1:30)*5 ## Library sizes
-        )
-
-## L8YTCKPF48Y070948 ## True
-## L8YTCKPF98Y070945 ## Pink slip
-## Outsite
+save_weighted_predictions(df,
+                          variables = variables,
+                          E = 3, ## Embedding dimension of the system.
+                          n_lags = 3, ## 0, -1,..., -(n_lags-1)
+                          n_samp = 150, ## Number of random libraries, should be in the hundreds
+                          lib = c(501,2001),  ## Library set.
+                          pred = c(2501,3000), ## Prediciton set.
+                          lib_sizes = (1:30)*5 ## Library sizes
+                          )
