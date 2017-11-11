950eaac8d110302044eac90947f5921a06c04404
Yair Daon <yair.daon@gmail.com>
2017-11-10 20:09:54 -0500

    snappy3's version

--- helpers/we.r
+++ helpers/we.r
@@ -1,11 +1,9 @@
 #!/usr/bin/Rscript
 library(rEDM)
 library(zoo)
-source( "helper.r" )
 
 weighted_colMeans <- function(X,
-                              V ) ## variances / uncertainties
-                              
+                              V) ## variances / uncertainties                          
 {
     W <- 1 / V ## Unnormalized weights 
     return( colSums(W * X) / colSums(W) ) ## Weighted average
@@ -26,7 +24,8 @@ we <- function(df, ## with lagged and scaled variables.
     ## Preallocate memory for everything.
     P <- matrix( NA,  nrow = n_comb, ncol = pred_size )
     V <- matrix( NA,  nrow = n_comb, ncol = pred_size )
-            
+    rhos        <- numeric( n_comb )
+    
     for( comb in 1:n_comb )
     {
         ## Variables used for prediction are chosen
@@ -47,10 +46,29 @@ we <- function(df, ## with lagged and scaled variables.
                              stats_only = FALSE )
         P[comb, ] <- output[[1]]$model_output$pred    [pred[1]:pred[2]] ## Prediction 
         V[comb, ] <- output[[1]]$model_output$pred_var[pred[1]:pred[2]] ## Uncertainty
-                
+
+        ## Now we do CV, for the MVE ranking. All we need
+        ## is the rho value for ranking.
+        cv_output <- block_lnlp(df,
+                                lib  = lib, 
+                                pred = lib, ## CV!!!
+                                method = "simplex",
+                                tp = 0, ## Lags built into df
+                                columns = cols,
+                                target_column = target,
+                                first_column_time = FALSE,
+                                stats_only = TRUE,
+                                silent = TRUE )
+        rhos[comb] <- cv_output$rho
+        
     } ## Closes for( comb in 1:n_comb )
 
+    ord  <- order( rhos, decreasing = TRUE )
+    best <- ord[ 1 : ceiling(sqrt(length(ord))) ]
+    P    <- P[ best, ]
+    V    <- V[ best, ]
     prediction <- weighted_colMeans(P,V)
+
     return( prediction )
            
 } ## Closes we <- function(...)
